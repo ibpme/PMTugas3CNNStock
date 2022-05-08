3fdf913c3325c31593c0c3d1ad47f8bca1b2bce1
Garry Kuwanto <gkuwanto@gmail.com>
2022-05-08 20:35:09 +0700

    Add EMA

--- technical_indicators/ema.py
+++ technical_indicators/ema.py
@@ -1,5 +1,12 @@
 import numpy as np
 import pandas as pd
 
-def EMA(data: pd.DataFrame) -> np.ndarray:
-    pass
+def print_pretty(array):
+    print('[')
+    for i in array:
+        print('   ', (str(i) if not np.isnan(i) else 'np.nan') + ',')
+    print(']')
+
+def EMA(data: pd.DataFrame, period:int = 10) -> np.ndarray:
+    ema = data['Close'].ewm(span=period).mean()
+    return ema
