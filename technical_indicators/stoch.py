035150af353c9cd2720a02c3a3a6d7ec3356d168
Garry Kuwanto <gkuwanto@gmail.com>
2022-05-12 10:34:42 +0700

    Add stochastic oscilator

--- technical_indicators/stoch.py
+++ technical_indicators/stoch.py
@@ -1,5 +1,16 @@
 import numpy as np
 import pandas as pd
 
-def STOCH(data: pd.DataFrame) -> np.ndarray:
-    pass
+def STOCH(data: pd.DataFrame) -> tuple(np.ndarray, np.ndarray):
+
+    high_roll = data["High"].rolling(periods).max()
+    low_roll = data["Low"].rolling(periods).min()
+    
+    # Fast stochastic indicator
+    num = data["Close"] - low_roll
+    denom = high_roll - low_roll
+    slow_k = (num / denom) * 100
+    
+    # Slow stochastic indicator
+    slow_d = data["%K"].rolling(3).mean()
+    return slow_k, slow_d
